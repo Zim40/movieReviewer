a2341db462763e923aec0cf0de00cff220a52ab7
User <michaelm810129@gmail.com>
2024-07-22 21:32:25 +0930

    [FIX]: production api connection

--- client/src/routes/login.jsx
+++ client/src/routes/login.jsx
@@ -12,7 +12,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:5000/api/users/login", {
+      const response = await fetch("/api/users/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
